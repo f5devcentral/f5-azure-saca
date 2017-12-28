e09079f3d7356a6455b6ea1a42347e1210fb984f
Eric Chen <eric.chen@f5.com>
2017-12-28 15:43:42 -0500

    add deploy;fix password

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+ansible-playbook deploy.yaml
 ansible-playbook setup.yaml
-ansible-playbook  -i ./azure_rm.py -e ansible_ssh_pass=$f5_password update-vip-udr.yaml
+ansible-playbook  -i ./azure_rm.py -e ansible_ssh_pass="{{lookup('file','.password.txt')|b64decode }}" update-vip-udr.yaml
+
 
