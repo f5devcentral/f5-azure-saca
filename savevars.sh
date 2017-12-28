7ae567f584964cef66e53114731db9659e444278
Eric Chen <eric.chen@f5.com>
2017-12-27 22:42:17 -0500

    populate automation host

---
+++ savevars.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+USER=$1
+PASSWORD=$2
+APPID=$3
+SECRET=$4
+TENANT=$5
+KEY1=$6
+KEY2=$7
+KEY3=$8
+KEY4=$9
+
+umask 007
+
+cd /home/$USER
+git clone -b devel https://github.com/chen23/f5-azure-scca.git /home/$USER/f5-azure-scca
+cd /home/$USER/f5-azure-scca
+
+cat > /home/$USER/f5-azure-scca/.password.txt <<EOF
+${PASSWORD}
+EOF
+
+cat > /home/$USER/f5-azure-scca/sp.json <<EOF
+{
+  "appId": "${APPID}",
+  "password": "${SECRET}",
+  "tenant": "${TENANT}"
+}
+EOF
+
+cat > /home/$USER/f5-azure-scca/keys.txt <<EOF
+$KEY1
+$KEY2
+$KEY3
+$KEY4
+EOF
+
+USER=$USER ./gen_env.py > env.sh
+
+chown -R $USER /home/$USER/f5-azure-scca
