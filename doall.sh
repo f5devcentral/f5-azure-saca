41b4358c7317b10227cd82d24ef04ba0e07bd846
Eric Chen <eric.chen@f5.com>
2017-12-28 15:44:13 -0500

    run all commands

---
+++ doall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd $HOME/f5-azure-scca
+source env.sh
+#env
+ansible-playbook deploy.yaml
+ansible-playbook setup.yaml
+ansible-playbook  -i ./azure_rm.py -e ansible_ssh_pass="{{lookup('file','.password.txt')|b64decode }}" update-vip-udr.yaml
+commands=`python grab_vars.py --debug|grep -E "az network vnet subnet update"`
+echo -e "$commands"
+sh -c "$commands"
